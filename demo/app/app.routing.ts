0efef46e475707175bd5c144b7e2b520319f203e
Sebastian Witalec <sebawita@gmail.com>
2017-02-23 12:34:27 +0000

    swapping " for '

--- demo/app/app.routing.ts
+++ demo/app/app.routing.ts
@@ -1,16 +1,16 @@
-import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { Routes } from '@angular/router';
 
 import { TestComponent } from './test/test.component';
 import { PetSearchComponent } from './pet-search/pet-search.component';
 import { PetDetailsComponent } from './pet-details/pet-details.component';
 
 const routes: Routes = [
-    // { path: "", redirectTo: "/test", pathMatch: "full" },
-    { path: "", redirectTo: "/petsearch", pathMatch: "full" },
-    { path: "test", component: TestComponent },
-    { path: "petsearch", children: [
+    // { path: '', redirectTo: '/test', pathMatch: 'full' },
+    { path: '', redirectTo: '/petsearch', pathMatch: 'full' },
+    { path: 'test', component: TestComponent },
+    { path: 'petsearch', children: [
         { path: '', component: PetSearchComponent },
         { path: ':id', component: PetDetailsComponent }
     ]},
