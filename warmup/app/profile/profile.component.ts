7eccae76851a6a68601df3938991293c13953d12
Sebastian Witalec <sebawita@gmail.com>
2017-02-24 20:05:49 +0000

    Lesson 1

---
+++ warmup/app/profile/profile.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-profile',
+  templateUrl: './profile/profile.component.html',
+  styleUrls: ['./profile/profile.component.css']
+})
+export class ProfileComponent {
+  profile: Profile;
+
+  constructor() {
+    this.profile = {
+      name: '',
+      password: '',
+      angularPro: false,
+      dob: new Date(),
+      codingPower: 1
+    }
+  }
+
+  save() {
+    console.log(JSON.stringify(this.profile, null, 2));
+  }
+
+  clear() {
+    this.profile.name = '';
+    this.profile.password = '';
+    this.profile.angularPro = false;
+    this.profile.dob = new Date();
+    this.profile.codingPower = 1;
+  }
+}
+
+export interface Profile {
+  name: string;
+  password: string;
+  angularPro: boolean;
+  dob: Date;
+  codingPower: number;
+}
