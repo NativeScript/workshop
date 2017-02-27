dce51ff1a1d0b9dff9bc26ddecf0a83d8b290812
Sebastian Witalec <sebawita@gmail.com>
2017-02-27 11:29:59 +0000

    Added navigation to self with PageRoute

--- warmup/app/navigation/rgb.component.ts
+++ warmup/app/navigation/rgb.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { RouterExtensions } from 'nativescript-angular'
+import { RouterExtensions, PageRoute } from 'nativescript-angular'
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
 
 @Component({
   selector: 'my-color-paletter',
@@ -11,16 +12,19 @@ export class RGBComponent implements OnInit{
   rgb: string = '#bad';
 
   constructor(
-    private route: ActivatedRoute,
+    private pageRoute: PageRoute,
     private router: RouterExtensions) {
   }
 
   ngOnInit() {
-    this.rgb = this.route.snapshot.params['rgb'];
+    this.pageRoute.activatedRoute
+      .switchMap(activatedRoute => activatedRoute.params)
+      .forEach(params => this.rgb = params['rgb']);
   }
 
   goBlue() {
     this.router.navigate(['/color/blue'], {
+    // this.router.navigate(['/color', '#00f'], {
       transition: {
         name: "slideLeft", // see http://docs.nativescript.org/api-reference/interfaces/_ui_frame_.navigationtransition.html#name
         duration: 2000,
@@ -31,12 +35,24 @@ export class RGBComponent implements OnInit{
 
   goRed() {
     this.router.navigate(['/color/red'], {
+    // this.router.navigate(['/color', '#f00'], {
       transition: {
         name: "slideTop",
         duration: 2000,
         curve: "easeOut"
       }
     });
+  }
+
+  goRandom() {
+    this.router.navigate(['/color', this.getRandomColor()]);
+  }
+
+  getRandomColor(): string {
+    const r = Math.floor(Math.random() * 16).toString(16);
+    const g = Math.floor(Math.random() * 16).toString(16);
+    const b = Math.floor(Math.random() * 16).toString(16);
+    return '#' + r+g+b;
   }
 
   goBack() {
