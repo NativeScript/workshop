10e958aa909266e3a5a46b4d40cf2460d0a46549
Sebastian Witalec <sebawita@gmail.com>
2017-03-12 15:18:30 +0000

    Reset to the beginning of the exercise

--- warmup/app/football/fixture.component.ts
+++ warmup/app/football/fixture.component.ts
@@ -7,28 +7,6 @@ import { Fixture } from '../models';
   styleUrls: ['./football/fixture.component.css']
 })
 export class FixtureComponent {
-  @Input() fixture: Fixture;
-  @Output() teamTap: EventEmitter<number> = new EventEmitter<number>();
 
-  constructor() {
-  }
 
-  public displayScore(): boolean {
-    return this.fixture.status === 'FINISHED' || this.fixture.status === 'IN_PLAY';
-    // other values TIMED, SCHEDULED
-  }
-
-  public inPlay(): boolean {
-    return this.fixture.status === 'IN_PLAY';
-  }
-
-  homeTeamTap() {
-    console.log('::FixtureComponent::homeTeamTap::' + this.fixture.homeTeamId);
-    this.teamTap.emit(this.fixture.homeTeamId);
-  }
-
-  awayTeamTap() {
-    console.log('::FixtureComponent::awayTeamTap::' + this.fixture.awayTeamId);
-    this.teamTap.emit(this.fixture.awayTeamId);
-  }
 }
