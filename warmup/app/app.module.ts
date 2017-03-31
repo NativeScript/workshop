170d9ad0404932e88319e3dda2a1e0d8bda34cc8
jlooper <jen.looper@gmail.com>
2017-03-31 12:05:34 -0400

    upgrading warmups to angular 4 :dancers:

--- warmup/app/app.module.ts
+++ warmup/app/app.module.ts
@@ -3,6 +3,7 @@ import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { NativeScriptHttpModule } from 'nativescript-angular/http';
+
 // import { NativeScriptFormsModule } from 'nativescript-angular/forms';
 
 //Lesson 1
--- warmup/app/main.ts
+++ warmup/app/main.ts
@@ -1,9 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic } from 'nativescript-angular/platform';
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
-import { AppModule } from './app.module';
-
-// import {registerElement} from "nativescript-angular/element-registry";
-// registerElement("PullToRefresh", () => require("nativescript-pulltorefresh").PullToRefresh);
+import { AppModule } from "./app.module";
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
