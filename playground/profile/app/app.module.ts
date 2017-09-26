12fc0cb29ee117b3ad42f95c6daaa7080a030a04
sebawita <sebawita@gmail.com>
2017-09-26 02:41:44 +0100

    Fixing single quotes

--- playground/profile/app/app.module.ts
+++ playground/profile/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 
-import { AppComponent } from "./app.component";
-import { ProfileComponent } from "./profile/profile.component";
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 
 import { NativeScriptUIDataFormModule } from 'nativescript-pro-ui/dataform/angular';
 // import { NativeScriptUIDataFormModule } from 'nativescript-telerik-ui-pro/dataform/angular';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
-// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+// import { NativeScriptFormsModule } from 'nativescript-angular/forms';
 
 
 @NgModule({
