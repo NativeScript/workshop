2017314c20794536f8164d110705c69de8355b52
sebawita <sebawita@gmail.com>
2017-09-28 01:01:49 +0200

    cleaning up

--- warmup/app/football.service.ts
+++ warmup/app/football.service.ts
@@ -5,7 +5,6 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw'
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise'
 
 @Injectable()
 export class FootballService {
